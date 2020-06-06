4543fe7c5e1e7bb9bc45bd66fb29f335de6d5248
evillase <edgar.villasenor@wsu.edu>
2020-06-06 13:30:59 -0700

    Problem 2

--- hw4/HW4-DB/HW4.sql
+++ hw4/HW4-DB/HW4.sql
@@ -8,3 +8,13 @@ WHERE Instructor.instructorID IN (
     SELECT instructorID 
     FROM Class 
     WHERE Major = 'CptS' AND Semester = 'Spring' AND Year = '2020');
+
+-- Problem 2
+SELECT Temp.classID, class.major, class.coursenum, Temp.numStudents 
+FROM (
+    SELECT Temp.classID, Temp.numStudents
+    FROM (SELECT classID, COUNT(classID) as numStudents 
+    FROM Enroll 
+    GROUP BY classID
+) AS TEMP) as Temp, Class
+WHERE Temp.classID = Class.classID AND Temp.numStudents > 10;
